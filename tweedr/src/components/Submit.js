7ef795187e04cf39fa9c158d57d4b45313688887
Kfrable <kcfrable@gmail.com>
2017-05-26 16:39:21 -0400

    working on rendering.

---
+++ tweedr/src/components/Submit.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+
+
+
+
+function Submit(props) {
+	return(
+		<div>
+			
+			<rounded />
+       
+      
+		
+		
+		<button onClick={props.onClick}>press</button>
+		</div>
+	);
+}
+
+export default Submit;
